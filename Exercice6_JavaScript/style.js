6ab5eb686390db806a72b949c11a191392e51c71
Valerie Jardin <valerie.jardin60@gmail.com>
2017-02-22 15:32:43 +0100

    Exercice 6 corrigé

--- Exercice6_JavaScript/style.js
+++ Exercice6_JavaScript/style.js
@@ -1,12 +1,14 @@
-function check(){
+function check(){ //Nom de la fonction non parlante, il aurrait était préférable de la nommer modulo.
 /*Déclaration des variables input*/
   var first = document.getElementById('firstNumber').value;
   var second = document.getElementById('secondNumber').value;
     //L'opérateur ! indique l'inverse de la valeur
-  if (!parseInt(first)||!parseInt(second)){
+  if (isNaN(first)||isNaN(second)){// || est un opérateur logique
     alert('Il faut des nombres');
+  }else if (second == 0){ // Mettre un double égal pour faire une comparaison.
+      alert('On ne peut pas diviser par zéro!');
   } else {
-    var result = parseInt(first)%parseFloat(second);
-    alert('Voici le résultat : ' + result);
+    var result = parseInt(first) % second;// % opérateur de division
+    alert('Voici le reste : ' + result);
   }
   }
