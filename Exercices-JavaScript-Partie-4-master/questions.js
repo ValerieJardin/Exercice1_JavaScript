ebfd4c6390d39c6a7bb9d3802a6319c7fd84f827
Valerie Jardin <valerie.jardin60@gmail.com>
2017-02-27 15:50:17 +0100

    Jasmine partie 4  terminée

--- Exercices-JavaScript-Partie-4-master/questions.js
+++ Exercices-JavaScript-Partie-4-master/questions.js
@@ -10,10 +10,10 @@ var remplaceECar = function (texte) {
     return texte.replace('e', ' ');
 }
 var concatString = function (texte1, texte2) {
-    return texte1.concat(texte2);
+    return texte1.concat(texte2); <!--texte1 + texte2-->
 }
 var afficherCar5 = function (texte) {
-    return 'texte'.charAt(4);
+    return texte.charAt(4); <!--Index 4-->
 }
 var afficher9Car = function (texte) {
     return texte.substring(0,9);
@@ -28,12 +28,12 @@ var SupprEspaceString = function (texte) {
     return texte.trim();
 }
 var IsString = function (texte) {
-  if (typeof texte == 'string') {
+  if (typeof texte == 'string') { <!--return typeof(texte) == 'string';-->
     return true;
   }
 }
 var AfficherExtensionString = function (texte) {
-    return texte.split('.').pop();
+    return texte.split('.').pop(); <!--transforme la chaine en tableau à chaque fois quil y a un point et le .pop va chercher la dernière ligne-->
 }
 var NombreEspaceString = function (texte) {
     return texte.split(' ').length -1;
@@ -61,5 +61,5 @@ var hypothenuse = function (ab, ac) {
     return Math.hypot(ab, ac);
 }
 var calculIMC = function (poids, taille) {
-    return Math.round(poids/Math.pow(taille,2),-2);
+    return Math.round(poids/Math.pow(taille,2)*100)/100;
 }
